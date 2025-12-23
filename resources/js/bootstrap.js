611fd1a2c22748f4c03420ce921926f969723def
dylaasdev <dylaasdev@gmail.com>
2025-12-24 01:37:27 +0800

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,8 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// --- ADD THESE LINES ---
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
+window.axios.defaults.baseURL = 'http://localhost:8000'; // Ensure this matches your backend URL
